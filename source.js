e124968643e6bd81126e8f87e476f70d02911313
Pam Selle <pamela.selle@gmail.com>
2020-09-07 13:25:31 -0400

    Big changes, generate patterns every 5 secs

--- source.js
+++ source.js
@@ -1,43 +1,70 @@
-// TODO draw the visualization
+function makePattern() {
+  // Create a pattern, offscreen
+  const patternCanvas = document.createElement("canvas");
+  const patternContext = patternCanvas.getContext("2d");
+
+  // Make some random values using the crypto API
+  var array = new Uint8Array(2);
+  window.crypto.getRandomValues(array);
+
+  // Give the pattern a width and height of 50
+  patternCanvas.width = array[0] / 2 > 15 ? array[0] / 2 : 10;
+  console.log("Pattern width", patternCanvas.width);
+  patternCanvas.height = array[1] / 2 > 5 ? array[1] / 2 : 10;
+  console.log("Pattern height:", patternCanvas.height);
+
+  // Give the pattern a background color and draw an arc
+  patternContext.fillStyle = "#eee";
+  patternContext.fillRect(0, 0, patternCanvas.width, patternCanvas.height);
+  patternContext.arc(0, 0, 25, 0, Math.cos(array[0]) * 2);
+  patternContext.strokeStyle = color;
+  patternContext.stroke();
+  return patternCanvas;
+}
 function drawViz(randomness) {
   // Sample randomness: 14cc25f8c8b7ffa8b47a036030f2d7ad0e97775a61c12752ad900d58de995a7e
-  // Ideas: Based on the numbers present in the value
-  // Based on the number of vowels in the value
   const regex = /[a-z]/gi;
-  r = randomness.replaceAll(regex, '');
-  r = Number(r)
-  r = Math.log(r).toFixed(2)
-  var canvas = document.getElementById('canvas');
-  if (canvas.getContext) {
-    var ctx = canvas.getContext('2d');
+  const colorRegex = /[g-z]/gi;
+  // Generate a number from the random value
+  r = randomness.replaceAll(regex, "");
+  // Generate a color from the random value
+  color = "#" + randomness.replaceAll(colorRegex, "").slice(0,6);
+  console.log("Color:", color);
+  r = Math.log(r).toFixed(2);
 
-    ctx.fillRect(25, 25, 100, r);
-    console.log(r)
-    ctx.clearRect(45, 45, 60, 60);
-    ctx.strokeRect(50, 50, 50, 50);
+  var canvas = document.getElementById("canvas");
+  if (canvas.getContext) {
+    var ctx = canvas.getContext("2d");
+
+    const pattern = ctx.createPattern(makePattern(color), "repeat");
+    ctx.fillStyle = pattern;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    // ctx.clearRect(45, 45, 60, 60);
+    // ctx.strokeRect(50, 50, 50, 50);
   }
 }
 
 // A global variable so we can abort the polling
-var cancelPoll = false
+var cancelPoll = false;
+var interval = 5;
 // set a window timeout to update the pattern based
 // on the value of data
 function poll() {
-  console.log('starting poll')
+  console.log("starting polling every " + interval + " seconds");
   const runPoll = function() {
     if (cancelPoll) {
-      return
+      return;
     }
     fetch("https://drand.cloudflare.com/public/latest")
-    .then(response => response.json())
-    .then(data => {
-      // Have the random data, run an update function
-      console.log(data.randomness)
-      drawViz(data.randomness)
-      setTimeout(runPoll, 2*1000)
-    });
-  }
-  return runPoll()
+      .then(response => response.json())
+      .then(data => {
+        // Have the random data, run an update function
+        console.log("Drand value: ", data.randomness);
+        drawViz(data.randomness);
+        setTimeout(runPoll, interval * 1000);
+      });
+  };
+  return runPoll();
 }
 
-poll()
+poll();
